f74fc25a5a66e6e473ead839129f27524a7e90df
Sebastian Schlosser <sebastian.schlosser@sens.at>
2024-11-14 10:06:18 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const PORT = process.env.HOST_PORT || process.env.PORT || 9000;
 app.get("/", function (req, res) {
 	const json = {
 		response:
-			"DEV_BRANCH - Hello From Thetips4you changed_new_" + process.env.TEST_NODE,
+			"DEV_BRANCH_FIX_ - Hello From Thetips4you changed_new_" +
+			process.env.TEST_NODE,
 	};
 	res.json(json);
 });
